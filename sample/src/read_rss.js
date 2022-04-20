e7017f0e9e264aed112695594ea475f44e16105e
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2022-04-20 13:38:57 +0900

    [fix] マルチプラットフォーム化

--- sample/src/read_rss.js
+++ sample/src/read_rss.js
@@ -1,6 +1,24 @@
+// モジュール読み込み
 const request = require('request');
+const path = require('path');
+const {platform,homedir} = require("os");
+const fs = require('fs');
 
-var URL = 'http://localhost:1212/soleil_api/run_function'; // 自分の使っているポートを指定
+// ポートを調べてくる
+var config_path = path.join(homedir(), "AppData", "Roaming", "terrescot", "config.json");
+switch (platform()) {
+    case "win32":
+        config_path = path.join(homedir(), "AppData", "Roaming", "terrescot", "config.json");
+        break;
+    case "darwin":
+        config_path = path.join(homedir(), "Library", "Application Support", "terrescot", "config.json");
+        break;
+    case "linux":
+        config_path = path.join(homedir(), ".config", "terrescot", "config.json");
+        break; 
+}
+var config = JSON.parse(fs.readFileSync(config_path, {encoding: 'utf-8'}));
+var URL = `http://localhost:${config.config.port}/soleil_api/run_function`; // 自分の使っているポートを指定
 
 request.post({
     uri: URL,
